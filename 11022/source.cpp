0a2bd5667d84a0af96ab2912f62984b56ec4cc3e
Bokyoung <bellbpng@gmail.com>
2021-04-25 15:30:06 +0900

    math

---
+++ 11022/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	int A, B;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		printf("Case #%d: %d + %d = %d\n", i + 1, A, B, A + B);
+	}
+}
