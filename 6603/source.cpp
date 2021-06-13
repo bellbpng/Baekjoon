b7d9f004de0e21c8720a33157c6257d2cd655805
Bokyoung <bellbpng@gmail.com>
2021-06-13 19:59:06 +0900

    math, back tracking, recursion

--- 6603/source.cpp
+++ 6603/source.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<int> lotto(13);
-vector<int> com(6);
 int k;
+int lotto[12] = { 0, };
+int comb[6];
 
 void dfs(int start, int depth) {
 	if (depth == 6) {
 		for (int i = 0; i < 6; i++) {
-			printf("%d ", com[i]);
+			printf("%d ", comb[i]);
 		}
 		printf("\n");
 		return;
 	}
-
 	for (int i = start; i < k; i++) {
-		com[depth] = lotto[i];
+		comb[depth] = lotto[i];
 		dfs(i + 1, depth + 1);
 	}
 }
 
-int main()
-{
-	while (true) {
-		cin >> k;
-		if (k == 0) break;
-
+int main() {
+	cin >> k;
+	while (k != 0) {
 		for (int i = 0; i < k; i++) {
 			cin >> lotto[i];
 		}
 		dfs(0, 0);
+		printf("\n");
+		cin >> k;
 	}
 }
