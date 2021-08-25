55951aece73e908c40b4a455a491a420868a7410
Bokyoung <bellbpng@gmail.com>
2021-08-25 17:58:06 +0200

    math, implement, string

---
+++ 5355/source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	getchar();
+	for (int i = 0; i < T; i++) {
+		string str;
+		getline(cin, str);
+		bool chk = false;
+		int idx = 0;
+		while (str[idx] != ' ') {
+			if (str[idx] == '.') chk = true;
+			idx++;
+		}
+		float num = 0.0;
+		if (chk) num = stof(str);
+		else num = float(stoi(str));
+		idx++;
+		for (;idx < str.length(); idx++) {
+			char ch = str[idx];
+			if (ch == '@') num *= 3;
+			else if (ch == '%') num += 5;
+			else if (ch == '#') num -= 7;
+		}
+		printf("%.2f\n", num);
+	}
+}
