de68e4af1c5c8773a746d2b51bbe9b8e3f2e85f9
Bokyoung <bellbpng@gmail.com>
2022-01-22 18:45:59 +0900

    math, dp

--- 9655/source.cpp
+++ 9655/source.cpp
@@ -5,14 +5,6 @@ int main()
 {
 	int N;
 	cin >> N;
-	if (N <= 6) {
-		if (N % 2 == 0) {
-			cout << "CY" << endl; return 0;
-		}
-		else {
-			cout << "SK" << endl; return 0;
-		}
-	}
 	int tmp = N % 6;
 	if (tmp % 2 == 1) cout << "SK" << endl;
 	else cout << "CY" << endl;
