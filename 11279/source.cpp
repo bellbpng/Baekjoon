3d8ccf745c3b3d8ad8d3cbd65d57ae296ca1de9d
Bokyoung <bellbpng@gmail.com>
2022-02-14 14:44:10 +0900

    data structure, priority queue

---
+++ 11279/source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int n;
+	std::cin >> n;
+	std::priority_queue<int> pq;
+	for (int i = 0; i < n; i++) {
+		int x;
+		std::cin >> x;
+		if (x == 0) {
+			if (pq.empty()) std::cout << "0\n";
+			else {
+				std::cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else pq.push(x);
+	}
+}
