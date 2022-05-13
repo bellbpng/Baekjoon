71ed6d2f86a6d7708a3a0caa69e2fc37b1bca2a2
Bokyoung <bellbpng@naver.com>
2022-05-13 14:02:02 +0900

    implementation, string

--- 5426/source.cpp
+++ 5426/source.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <cmath>
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-
+	std::ios_base::sync_with_stdio(false);
 	int t = 0;
 	std::cin >> t;
 	for (int i = 0; i < t; i++) {
-		std::string str;	
+		std::string str;
 		std::cin >> str;
-		const int len = str.length();
-		const int border = std::sqrt(len);
-
-		std::vector<std::vector<char>> letter(border, std::vector<char>(border));
-		int k = 0;
-
-		for (int row = 0; row < border; row++) {
-			for (int col = 0; col < border; col++) {
-				letter[row][col] = str[k++];
-			}
-		}
-
-		for (int col = border - 1; col >= 0; col--) {
-			for (int row = 0; row < border; row++) {
-				std::cout << letter[row][col];
-			}
-		}
+		int len = str.length();
+		int lim = std::sqrt(len);
+		for (int k = 0; k < lim; k++)
+			for (int l = lim - 1; l < len; l += lim)
+				std::cout << str[l - k];
 		std::cout << "\n";
 	}
 }
